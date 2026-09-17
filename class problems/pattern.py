7a686c6fc6dda8be2c9dbad347c1e6a7f3569107
jmvibushana03 <jmvibushana03@gmail.com>
2026-09-17 14:36:34 +0530

    Added week 4 problems

--- class problems/pattern.py
+++
@@ -1,7 +0,0 @@
-rows = int(input())
-print(f"The right-angled triangle pattern for {rows} rows is")
-
-for i in range(1, rows + 1):
-    for j in range(1, i + 1):
-        print("*", end="")
-    print()
